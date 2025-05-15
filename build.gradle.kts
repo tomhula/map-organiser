f2398b2ab0383d947d7b2f3a357b49a800230ff0
Tomas Hula <tomashula06@gmail.com>
2025-05-15 10:33:27 +0200

    Create municipality index

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,10 @@ dependencies {
     implementation("io.github.tomhula:orisclient-jvm:0.0.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("io.github.g0dkar:qrcode-kotlin:4.4.1")
-    // Freemarker dependencies
+    implementation("com.google.code.gson:gson:2.13.1")
     implementation("org.freemarker:freemarker:2.3.34")
+    implementation("io.ktor:ktor-client-core:3.1.3")
+    implementation("io.ktor:ktor-client-java:3.1.3")
 
     testImplementation(kotlin("test"))
 }
--- src/main/kotlin/io/github/tomhula/Main.kt
+++ src/main/kotlin/io/github/tomhula/Main.kt
@@ -1,23 +1,33 @@
 package io.github.tomhula
 
+import com.google.gson.JsonParser
 import freemarker.template.Configuration
 import freemarker.template.TemplateExceptionHandler
 import io.github.tomhula.orisclient.Oris
 import io.github.tomhula.orisclient.OrisImpl
 import io.github.tomhula.orisclient.dto.Event
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.java.Java
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpHeaders
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.format
 import kotlinx.datetime.format.Padding
 import kotlinx.datetime.format.char
+import kotlinx.datetime.minus
 import qrcode.QRCode
 import java.io.File
 import java.io.StringWriter
 import kotlin.io.encoding.Base64
 import kotlin.io.encoding.ExperimentalEncodingApi
+import kotlin.time.Duration.Companion.seconds
 
 
 val oris: Oris = OrisImpl()
@@ -35,16 +45,66 @@ val dateFormat = LocalDate.Format {
     char('.')
     year()
 }
+val httpClient = HttpClient(Java)
 
 fun main(args: Array<String>) = runBlocking {
     val userRegNum = args.getOrNull(0) ?: throw RuntimeException("User registration number must be provided")
     val outputFile = args.getOrNull(1) ?: "event_grid.html"
 
+    println("Downloading events of user $userRegNum...")
     val events = getUserEvents(userRegNum)
-    val gridHtml = renderGrid(events)
 
+    println("Generating event grid...")
+    val gridHtml = renderGrid(events)
     File(outputFile).writeText(gridHtml)
     println("Event grid of ${events.size} events has been generated to $outputFile")
+    
+    
+    println("Generating municipality index...")
+    val eventsByMunicipality = groupEventsByMunicipality(events)
+    val eventsByMunicipalityOrdered = eventsByMunicipality.toSortedMap(compareBy { it?.lowercase() })
+
+    val eventsMunicipalityIndex = buildString {
+        for ((municipality, municipalityEvents) in eventsByMunicipalityOrdered)
+        {
+            appendLine("## ${municipality ?: "N/A"}")
+            for (event in municipalityEvents)
+            {
+                val eventNumber = events.indexOf(event) + 1
+                appendLine("$eventNumber: ${event.place}  ")
+            }
+        }
+    }
+
+    File("events_by_municipality.md").writeText(eventsMunicipalityIndex)
+}
+
+private suspend fun groupEventsByMunicipality(events: Iterable<Event>): Map<String?, List<Event>>
+{
+    val eventsWithMunicipality = events.associateWith { event ->
+        if (event.gPSLat == null || event.gPSLat == "0" || event.gPSLon == null || event.gPSLon == "0")
+            return@associateWith null
+
+        val municipality = runCatching {
+            val response =
+                httpClient.get("https://nominatim.openstreetmap.org/reverse?format=json&lat=${event.gPSLat}&lon=${event.gPSLon}") {
+                    header(HttpHeaders.UserAgent, "https://github.com/tomhula/map-organiser")
+                }.bodyAsText()
+            val json = JsonParser.parseString(response).asJsonObject
+            json["address"].asJsonObject["municipality"].asString
+        }
+        
+        if (municipality.isFailure)
+            println("Failed to get municipality for https://nominatim.openstreetmap.org/reverse?format=json&lat=${event.gPSLat}&lon=${event.gPSLon}")
+
+        delay(1.seconds)
+        municipality.getOrNull()
+    }
+
+
+    val eventsByMunicipality = eventsWithMunicipality.keys.groupBy { event -> eventsWithMunicipality[event] }
+
+    return eventsByMunicipality
 }
 
 private suspend fun getUserEvents(userRegNum: String): List<Event>
